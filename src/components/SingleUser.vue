b5bc413928e41ba0458098e313686c16ac5365f9
shamil-am <shamil.mammadov1@gmail.com>
2022-03-13 16:43:37 +0400

    crud with vue js

---
+++ src/components/SingleUser.vue
@@ -0,0 +1,23 @@
+<template>
+  <tr>
+    <td>{{ person.name }}</td>
+    <td>{{ person.username }}</td>
+    <td>{{ person.company.name }}</td>
+    <td>{{ person.address.city }}</td>
+    <td>{{ person.address.street }}</td>
+    <td>{{ person.phone }}</td>
+    <td>
+      <button class="btn btn-danger" @click="removeUser(user)">X</button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "SingleUser",
+  props: ["person"],
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,42 @@
+<template>
+  <p v-if="userList.length === 0">List is empty</p>
+  <table class="table table-info" v-else>
+    <thead>
+      <th>Name</th>
+      <th>Username</th>
+      <th>Company</th>
+      <th>City</th>
+      <th>Address</th>
+      <th>Phone</th>
+      <th></th>
+    </thead>
+    <tbody>
+      <SingleUser
+        v-for="user in userList"
+        :key="user.id"
+        :person="user"
+      ></SingleUser>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import SingleUser from "./SingleUser.vue";
+export default {
+  name: "UserList",
+  components: {
+    SingleUser,
+  },
+  props: {
+    userList: Array,
+  },
+  methods: {
+    removeUser(user) {
+      this.$emit("remove-user", user);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
